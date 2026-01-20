85693187088b496309c453803753e9a30befcfee
Pofkinas <povilaszvi358@gmail.com>
2026-01-20 23:31:32 +0200

    feat(stm32/thirdparty/FreeRTIOSConfig): enable FPU and increase heap

--- stm32/ThirdParty/Core/Inc/FreeRTOSConfig.h
+++ stm32/ThirdParty/Core/Inc/FreeRTOSConfig.h
@@ -58,7 +58,7 @@
 #define CMSIS_device_header "stm32f4xx.h"
 #endif /* CMSIS_device_header */
 
-#define configENABLE_FPU                         0
+#define configENABLE_FPU                         1
 #define configENABLE_MPU                         0
 
 #define configUSE_PREEMPTION                     1
@@ -70,7 +70,7 @@
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 56 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)15360)
+#define configTOTAL_HEAP_SIZE                    ((size_t)20480)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
 #define configUSE_TRACE_FACILITY                 1
 #define configUSE_16_BIT_TICKS                   0
@@ -173,6 +173,7 @@ standard names. */
 /* Definitions needed when configGENERATE_RUN_TIME_STATS is on */
 #define portCONFIGURE_TIMER_FOR_RUN_TIME_STATS configureTimerForRunTimeStats
 #define portGET_RUN_TIME_COUNTER_VALUE getRunTimeCounterValue
+#define configUSE_TASK_FPU_SUPPORT 2
 /* USER CODE END Defines */
 
 #endif /* FREERTOS_CONFIG_H */
